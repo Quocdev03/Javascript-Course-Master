cf12da3f60c37e8ade2fac9c834f7a42c47119be
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-02-25 16:40:37 +0700

    update

--- dom/insert.js
+++ dom/insert.js
@@ -1,4 +1,4 @@
-// 1. insertAdjacenText
+// 1. element.insertAdjacenText("position", "text")
 const h3 = document.querySelector("h3");
 // h3.insertAdjacentText("position", "text")
 // position: beforebegin, afterbegin, beforeend, afterend
@@ -6,4 +6,24 @@ h3.insertAdjacentText("beforebegin", "beforebegin");
 h3.insertAdjacentText("afterbegin", "afterbegin");
 h3.insertAdjacentText("beforeend", "beforeend");
 h3.insertAdjacentText("afterend", "afterend");
-// 2. ins
+// 2. element.insertAdjacentElement("position", node)
+const strong = document.createElement("strong");
+strong.classList.add("bold");
+h3.insertAdjacentElement("beforeend", strong);
+// 3. element.insertAdjacentHTML
+/*
+<ul class ="menu">
+<li>1</li>
+<li>2</li>
+<li>3</li>
+</ul>
+*/
+
+const template = `
+<ul class ="menu2">
+   <li>1</li>
+   <li>2</li>
+   <li>3</li>
+</ul>`;
+document.body.insertAdjacentHTML("beforeend", template)
+// parse
