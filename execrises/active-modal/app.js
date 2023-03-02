f55cc3eac83abc08a91bfca25c208dbc8a95f459
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-03-02 12:36:57 +0700

    update

--- execrises/active-modal/app.js
+++ execrises/active-modal/app.js
@@ -1,51 +1,39 @@
 /*
-   
-*/
+ */
 const modal = document.createElement("div");
 modal.classList.add("modal");
 const modalContent = document.createElement("div");
-modalContent.classList.add("modal-content")
+modalContent.classList.add("modal-content");
 modal.appendChild(modalContent);
 const modalClose = document.createElement("i");
 modalClose.className = "fa fa-times modal-close";
 modalContent.appendChild(modalClose);
-const modalDesc = document.createElement("p")
-modalDesc.className = "modal-desc";
-modalDesc.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa debitis beatae officiis ex esse sint eveniet voluptatum enim dolores? Distinctio, amet. Facere dolorem laboriosam quidem, sapiente consequuntur tempora culpa ullam?";
+const modalDesc = document.createElement("div");
+modalDesc.classList.add("modal-desc");
+modalDesc.textContent =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur, corporis neque! Molestias voluptates commodi doloremque eius, aut tempore et tenetur expedita totam molestiae maxime culpa nihil sapiente quos rerum error?";
 modalContent.appendChild(modalDesc);
-const modalAction = document.createElement("div");
-modalAction.className = "modal-action";
-const modalSubmit = document.createElement("button");
-modalSubmit.className = "modal-submit";
-const modalCancel = document.createElement("button");
-modalCancel.className = "modal-cancel";
-modalSubmit.textContent = "Confirm";
-modalCancel.textContent = "Cancel";
-modalAction.appendChild(modalSubmit);
-modalAction.appendChild(modalCancel);
-modalContent.appendChild(modalAction);
 document.body.appendChild(modal);
-// const template = `
-// <div class="modal">
+// const template = `<div class="modal">
 // <div class="modal-content">
-//    <i class="modal-close fa fa-times"></i>
-//    <p class="modal-desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-//       debitis beatae officiis ex esse sint eveniet voluptatum enim dolores? Distinctio, amet.
-//       Facere dolorem laboriosam quidem, sapiente consequuntur tempora culpa ullam?
-//    <div class="modal-action">
-//       <button class="modal-submit">Confirm</button>
-//       <button class="modal-cancel">Cancel</button>
-//    </div>
-//    </p>
+// <i class="fa fa-times modal-close"></i>
+// <div class="modal-desc">
+//   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+//   iure veniam reiciendis, accusamus dolorum exercitationem voluptates?
+//   Eos a recusandae consectetur non eveniet quam, id atque? Atque ex
+//   impedit quam eveniet!
+// </div>
+// <div class="modal-action">
+//   <button class="modal-submit">Confirm</button>
+//   <button class="modal-cancel">Cancel</button>
+// </div>
 // </div>
 // </div>`;
 // const body = document.body;
 // body.insertAdjacentHTML("afterbegin", template);
-
-const modalwrapper = document.querySelector(".modal");
-if (modalwrapper) {
-   setTimeout(function () {
-      modalwrapper.classList.add("is-show");
-   }, 2000)
+const modalWrapper = document.querySelector(".modal");
+if (modalWrapper) {
+  setTimeout(function () {
+    modalWrapper.classList.add("is-show");
+  }, 3000);
 }
-// tới 138
---
+++ execrises/fomo-notification/starter.js
@@ -0,0 +1,30 @@
+function createNotification(title = "Welcome to notification") {
+   const template = `<div class="noti">
+   <img src="https://source.unsplash.com/random" alt="" class="noti-image" />
+   <div class="noti-content">
+      <h3 class="noti-title">${title}</h3>
+      <p class="noti-desc">
+         Lorem ipsum dolor sit amet consec tetur, adipisicing elit. Quisquam
+         dolor sit amet consec
+      </p>
+   </div>
+</div>`;
+   document.body.insertAdjacentHTML("afterbegin", template);
+}
+let lassTitle;
+const randomData = [
+   "Welcome to javascript course",
+   "Welcome to this tutorial",
+   "Today is a good",
+   "My name is evondev",
+   "I am frontend developer"];
+const timer = setInterval(function () {
+   const item = document.querySelector(".noti");
+   if (item) item.parentNode.removeChild(item);
+   // lấy ra random title
+   const title = randomData[(Math.floor(Math.random() * randomData.length))]
+   if (lassTitle !== title) {
+      createNotification(title);
+   }
+   lassTitle = title;
+}, 5000)
