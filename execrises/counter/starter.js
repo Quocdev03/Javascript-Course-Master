6e8a032a69ecc262d0b69274f6e25832c3b61122
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-03-08 11:37:52 +0700

    update counter execrises

---
+++ execrises/counter/starter.js
@@ -0,0 +1,21 @@
+const buttonMinus = document.querySelector(".counter-descrease");
+const buttonPlus = document.querySelector(".counter-increase");
+const counterNumber = document.querySelector(".counter-number");
+let counterValue = counterNumber.textContent;
+console.log(counterValue)
+buttonPlus.addEventListener("click", function () {
+   if (counterValue >= 10) {
+      return 10;
+   }
+   counterValue++;
+   counterNumber.textContent = counterValue;
+   // counterValue = counterValue + 1;
+   // counterValue += 1;
+})
+buttonMinus.addEventListener("click", function () {
+   if (counterValue <= 0) return 0;
+   counterValue--;
+   counterNumber.textContent = counterValue;
+   // counterValue = counterValue - 1;
+   // counterValue -= 1;
+})
