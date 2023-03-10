e206d27bb4f8920e7e19cb6628255b8bcd7f5341
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-03-10 20:38:00 +0700

    update execrise tab

--- execrises/tab/starter.js
+++ execrises/tab/starter.js
@@ -1,10 +1,20 @@
-const tabItems = document.querySelectorAll(".tab-item");
-[...tabItems].forEach((item) => item.addEventListener('click', handleTabCLick));
-function handleTabCLick(event) {
-   console.log(event.target);
-   // Xoá hết tất cả class active ở các tab-item
-   [...tabItems].forEach(item => item.classList.remove("active"));
+const tabItem = document.querySelectorAll(".tab-item");
+const tabContent = document.querySelectorAll(".tab-content");
+// Chuyển tabItem thành mảng
+[...tabItem].forEach(item => item.addEventListener('click', handleTabClick));
+function handleTabClick(event) {
+   // Xoá hết tất cả class active ở các tab item
+   [...tabItem].forEach(item => item.classList.remove("active"));
    // sau đó add class active vào tab vừa click
    event.target.classList.add("active");
+   // Phần tabContent
+   // Lấy data-tab từ nút button
+   const tabNumber = event.target.dataset.tab;
+   [...tabContent].forEach(item => {
+      item.classList.remove("active");
+      if (item.getAttribute("data-tab") === tabNumber) {
+         item.classList.add("active");
+      }
+   });
+   // [...tabContent][tabNumber-1].classList.add("active");
 }
-// tới 161
