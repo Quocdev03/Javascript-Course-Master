0004191e2c19861deb17365517bc5be2b9df1315
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-03-13 20:56:03 +0700

    update

--- execrises/lightbox/starter.js
+++ execrises/lightbox/starter.js
@@ -1,16 +1,59 @@
 /*
 
 */
+// const images = document.querySelectorAll(".content img");
+// images.forEach((item) => item.addEventListener("click", handleZoomImage));
+// function handleZoomImage(event) {
+//    const image = event.target.getAttribute("src");
+//    const template =
+//       `<div class="lightbox">
+//          <div class="lightbox-content">
+//          <i class="fa fa-angle-left lightbox-prev"></i>
+//             <img class="lightbox-image"
+//             src="${image}"
+//             alt="">
+//          <i class="fa fa-angle-right lightbox-next"></i>
+//          </div>
+//       </div>`;
+//    document.body.insertAdjacentHTML("beforeend", template);
+// }
+// let index = 0;
+// document.body.addEventListener("click", function (e) {
+//    const lightImage = document.querySelector(".lightbox-image");
+//    let lightSrc = "";
+//    if (e.target.matches(".lightbox")) {
+//       e.target.parentNode.removeChild(e.target);
+//    } else if (e.target.matches(".lightbox-next")) {
+//       lightSrc = lightImage.getAttribute("src");
+//       index = [...images].findIndex((item) => item.getAttribute("src") === lightSrc);
+//       index = index + 1;
+//       if (index > images.length - 1) {
+//          index = 0;
+//       }
+//       const newSrc = [...images][index].getAttribute("src");
+//       lightImage.setAttribute("src", newSrc);
+//    } else if (e.target.matches(".lightbox-prev")) {
+//       lightSrc = lightImage.getAttribute("src");
+//       index = [...images].findIndex((item) => item.getAttribute("src") === lightSrc);
+//       index = index - 1;
+//       if (index < 0) {
+//          index = images.length - 1;
+//       }
+//       const newSrc = [...images][index].getAttribute("src");
+//       lightImage.setAttribute("src", newSrc);
+//    }
+// });
+
 const images = document.querySelectorAll(".content img");
-images.forEach((item) => item.addEventListener("click", handleZoomImage));
-function handleZoomImage(event) {
-   const image = event.target.getAttribute("src");
-   const template =
-      `<div class="lightbox">
+[...images].forEach((item) => item.addEventListener('click', handleZoomImage));
+function handleZoomImage(e) {
+   const image = e.target.getAttribute("src");
+   const template = `
+   <div class="lightbox">
          <div class="lightbox-content">
          <i class="fa fa-angle-left lightbox-prev"></i>
-            <img class="lightbox-image" 
-            src="${image}" 
+            <img class="lightbox-image"
+            src="${image}"
             alt="">
          <i class="fa fa-angle-right lightbox-next"></i>
          </div>
@@ -18,7 +61,7 @@ function handleZoomImage(event) {
    document.body.insertAdjacentHTML("beforeend", template);
 }
 let index = 0;
-document.body.addEventListener("click", function (e) {
+document.body.addEventListener('click', function (e) {
    const lightImage = document.querySelector(".lightbox-image");
    let lightSrc = "";
    if (e.target.matches(".lightbox")) {
@@ -43,4 +86,3 @@ document.body.addEventListener("click", function (e) {
       lightImage.setAttribute("src", newSrc);
    }
 });
-// tới 171
