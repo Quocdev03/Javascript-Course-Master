ee8018fd97af57f86af977511ae5999dfb162bf5
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-04-07 11:08:22 +0700

    update

---
+++ event/other.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//    console.log("loaded");
+// });
+const button = document.querySelector(".button");
+// removeEventListener
+function handleMouseMove(e) {
+   console.log(e.clientX);
+}
+document.addEventListener("mousemove", handleMouseMove);
+button.addEventListener("click", function (e) {
+   document.removeEventListener("mousemove", handleMouseMove);
+});
