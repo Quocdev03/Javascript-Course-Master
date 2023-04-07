c056201ec950a28f85b8067d585ea3d1bb97a512
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-04-07 11:19:54 +0700

    update

--- event/other.js
+++ event/other.js
@@ -3,10 +3,26 @@
 // });
 const button = document.querySelector(".button");
 // removeEventListener
-function handleMouseMove(e) {
-   console.log(e.clientX);
+// function handleMouseMove(e) {
+//    console.log(e.clientX);
+// }
+// document.addEventListener("mousemove", handleMouseMove);
+// button.addEventListener("click", function (e) {
+//    document.removeEventListener("mousemove", handleMouseMove);
+// });
+function handleClick() {
+   console.log("clicked1");
 }
-document.addEventListener("mousemove", handleMouseMove);
-button.addEventListener("click", function (e) {
-   document.removeEventListener("mousemove", handleMouseMove);
+function handleClick2() {
+   console.log("clicked2");
+}
+// event handler
+// onclick: chấp nhận 1 event handler
+// button.onclick = handleClick;
+// button.onclick = handleClick2;
+// addEventListener: chấp nhận nhiều event handler cùng lúc
+// button.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick2, {
+   // chỉ chạy 1 lần duy nhất
+   once: true,
 });
