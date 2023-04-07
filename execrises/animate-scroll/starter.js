a41ead8a2931ea36e3cb46457ef52fb7fa74cf2c
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-04-07 20:13:13 +0700

    update animation scroll

---
+++ execrises/animate-scroll/starter.js
@@ -0,0 +1,26 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//    const images = document.querySelectorAll(".container img");
+//    window.addEventListener("scroll", function (e) {
+//       const windowScrollTop = window.pageYOffset;
+//       [...images].forEach((item) => {
+//          if (windowScrollTop > item.offsetTop - (item.offsetHeight / 1.5)) {
+//             item.classList.add("active");
+//          } else {
+//             item.classList.remove("active");
+//          }
+//       });
+//    });
+// });
+window.addEventListener("DOMContentLoaded", function () {
+   const images = document.querySelectorAll(".container img");
+   window.addEventListener("scroll", function (e) {
+      const windowScrollTop = window.pageYOffset;
+      [...images].forEach((item) => {
+         if (windowScrollTop > item.offsetTop - (item.offsetHeight / 1.7)) {
+            item.classList.add("active");
+         } else {
+            item.classList.remove("active");
+         }
+      });
+   });
+});
