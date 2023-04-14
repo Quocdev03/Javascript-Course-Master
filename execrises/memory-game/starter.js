24f5ff7e7e5a6630cce9b9a4d6cd1b6a187b2a1b
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-04-14 14:46:15 +0700

    update

--- execrises/memory-game/starter.js
+++ execrises/memory-game/starter.js
@@ -32,39 +32,80 @@ const cardsArray = [
       img: "img/burger.png",
    },
 ];
+
 const grid = document.querySelector(".grid");
 let count = 0;
 let previousCards;
+let firstGuess = "";
+let secondGuess = "";
+const delay = 500;
+
 // .sort(() => 0.5 - Math.random()): random array
+
 const cardsArrayMerge = cardsArray.concat(cardsArray).sort(() => 0.5 - Math.random());
 cardsArrayMerge.forEach((item) => {
    const card = document.createElement("div");
    card.classList = "card";
    card.setAttribute("data-name", item.name);
+   // Front card
+   const front = document.createElement("div");
+   front.classList.add("front");
+   // Back card
+   const back = document.createElement("div");
+   back.classList.add("front");
    // card.dataset.name = item.name;
-   card.style.backgroundImage = `url(${item.img})`;
+   card.style.backgroundImage = `url(${item.img}) `;
+   card.style.cursor = "pointer";
+   card.appendChild(front);
+   card.appendChild(back);
    grid.appendChild(card);
 });
-// grid.addEventListener("click", function (e) {
-//    const clicked = e.target;
-//    if (clicked.nodeName === "SECTION" || previousCards === clicked) {
-//       return;
-//    }
-//    if (count < 2) {
-//       count++;
-//       clicked.classList.add("selected");
-//    }
-//    previousCards = clicked;
-// });
+
+function matchingCard() {
+   const selects = document.querySelectorAll(".selected");
+   [...selects].forEach((item) => item.classList.add("matched"));
+}
+
+function resetGuess() {
+   count = 0;
+   firstGuess = "";
+   secondGuess = "";
+   const selects = document.querySelectorAll(".selected");
+   [...selects].forEach((item) => item.classList.remove("selected"));
+}
+
 grid.addEventListener("click", function (e) {
    const clicked = e.target;
+
    if (clicked.nodeName === "SECTION" || previousCards === clicked) {
       return;
    }
-   if (count < 2) {
-      clicked.classList.add("selected");
-      count += 1;
-   }
-   previousCards = clicked;
 
+   if (count < 2) {
+      count++;
+
+      if (count === 1) {
+         firstGuess = clicked.parentNode.dataset.name;
+         clicked.parentNode.classList.add("selected");
+      } else {
+         secondGuess = clicked.parentNode.dataset.name;
+         clicked.parentNode.classList.add("selected");
+      }
+
+      if (firstGuess !== "" && secondGuess !== "") {
+
+         if (firstGuess === secondGuess) {
+            // handle match here
+            setTimeout(matchingCard, delay);
+            setTimeout(resetGuess, delay);
+         } else {
+            setTimeout(resetGuess, delay);
+         }
+
+      }
+
+   }
+
+   previousCards = clicked;
 });
+// 274
