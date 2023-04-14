04141588c7457a9701174be348b392cd65c71b96
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-04-14 15:04:46 +0700

    update

--- execrises/memory-game/starter.js
+++ execrises/memory-game/starter.js
@@ -41,26 +41,29 @@ let secondGuess = "";
 const delay = 500;
 
 // .sort(() => 0.5 - Math.random()): random array
-
-const cardsArrayMerge = cardsArray.concat(cardsArray).sort(() => 0.5 - Math.random());
-cardsArrayMerge.forEach((item) => {
-   const card = document.createElement("div");
-   card.classList = "card";
-   card.setAttribute("data-name", item.name);
-   // Front card
-   const front = document.createElement("div");
-   front.classList.add("front");
-   // Back card
-   const back = document.createElement("div");
-   back.classList.add("front");
-   // card.dataset.name = item.name;
-   card.style.backgroundImage = `url(${item.img}) `;
-   card.style.cursor = "pointer";
-   card.appendChild(front);
-   card.appendChild(back);
-   grid.appendChild(card);
-});
-
+function generateCard() {
+   // reset inner html
+   grid.innerHTML = "";
+   const cardsArrayMerge = cardsArray.concat(cardsArray).sort(() => 0.5 - Math.random());
+   cardsArrayMerge.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList = "card";
+      card.setAttribute("data-name", item.name);
+      // Front card
+      const front = document.createElement("div");
+      front.classList.add("front");
+      // Back card
+      const back = document.createElement("div");
+      back.classList.add("front");
+      // card.dataset.name = item.name;
+      card.style.backgroundImage = `url(${item.img}) `;
+      card.style.cursor = "pointer";
+      card.appendChild(front);
+      card.appendChild(back);
+      grid.appendChild(card);
+   });
+}
+generateCard();
 function matchingCard() {
    const selects = document.querySelectorAll(".selected");
    [...selects].forEach((item) => item.classList.add("matched"));
@@ -70,14 +73,22 @@ function resetGuess() {
    count = 0;
    firstGuess = "";
    secondGuess = "";
+   previousCards = null;
    const selects = document.querySelectorAll(".selected");
+   const matchedAll = document.querySelectorAll(".matched");
+   const cardLenght = document.querySelectorAll(".card").length;
    [...selects].forEach((item) => item.classList.remove("selected"));
+   if (matchedAll.length === cardLenght) {
+      // done game -> reset game
+      setTimeout(matchedAll.forEach((el) => el.classList.remove("matched")), delay);
+      setTimeout(generateCard, delay);
+   }
 }
 
 grid.addEventListener("click", function (e) {
    const clicked = e.target;
 
-   if (clicked.nodeName === "SECTION" || previousCards === clicked) {
+   if (clicked.nodeName === "SECTION" || previousCards === clicked || clicked.parentNode.classList.contains("selected") || clicked.parentNode.classList.contains("matched")) {
       return;
    }
 
@@ -92,20 +103,18 @@ grid.addEventListener("click", function (e) {
          clicked.parentNode.classList.add("selected");
       }
 
-      if (firstGuess !== "" && secondGuess !== "") {
+      if (firstGuess && secondGuess) {
 
          if (firstGuess === secondGuess) {
             // handle match here
             setTimeout(matchingCard, delay);
-            setTimeout(resetGuess, delay);
-         } else {
-            setTimeout(resetGuess, delay);
          }
+         setTimeout(resetGuess, delay);
 
       }
 
+      previousCards = clicked;
    }
 
-   previousCards = clicked;
 });
 // 274
