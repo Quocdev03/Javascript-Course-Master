23a4f8af3530c17f8e75c5d30a0d3fdf9922ba6c
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-04-16 11:27:26 +0700

    update params

---
+++ advanced/bom.js
@@ -0,0 +1,18 @@
+// Brower Object Modal: alert, prompt, confirm, window
+// 1. Location
+console.log(location);
+console.log(window.location);
+// setTimeout(() => {
+//    location.href = "https://evon.dev";
+// }, 1000);
+let params = new URLSearchParams(location.search);
+console.group(params);
+console.log(params.get("type"));
+console.log(params.has("page"));
+console.log(params.set("page", 10));
+console.log(params.get("page"));
+console.log(params.keys());
+for (let it of params.values()) {
+   console.log(it);
+}
+params.delete("page");
