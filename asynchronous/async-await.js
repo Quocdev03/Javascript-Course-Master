3d045baf6b36af11cb6121442c5b74d05a6abb79
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-05-19 18:13:29 +0700

    complete async await

--- asynchronous/async-await.js
+++ asynchronous/async-await.js
@@ -18,3 +18,42 @@ const student = {
    async calcAge() { },
    yearly: async () => { },
 }
+
+function makeTimer(timer = 1000, str) {
+   return new Promise(function (resole, reject) {
+      setTimeout(() => {
+         resole(str);
+      }, timer);
+   })
+}
+async function allTimer() {
+   const timer1 = makeTimer(1000, "first time");
+   const timer2 = makeTimer(2000, "second time");
+   const [a, b] = await Promise.all([timer1, timer2]);
+   console.log(a, b);
+   // const timer3 = makeTimer(3000, "third time");
+   // console.log(timer1);
+   // console.log(timer2);
+   // console.log(timer3);
+}
+// allTimer();
+function isFrontendDev(language) {
+   return new Promise(function (resole, reject) {
+      if (!language.includes("html")) {
+         reject("you are front end developer");
+      }
+      setTimeout(() => {
+         resole("you are not front end developer");
+      }, 1000)
+   });
+}
+async function go() {
+   const dev1 = await isFrontendDev(["css"]);
+   console.log(dev1);
+   return dev1;
+}
+// console.log(go());
+go()
+   .then()
+   .catch((err) => console.log(err));
+
