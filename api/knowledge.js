389ad78526e168cd4673e39404dd27558c2ac3eb
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-05-27 19:09:27 +0700

    xong bài lấy data từ api

---
+++ api/knowledge.js
@@ -0,0 +1,5 @@
+// cors: Cross Origin resoure sharing
+// same origin
+// same origin: evondev.com evondev.com/post
+// not same origin: evondev.com facebook.com
+// 338
--- api/picsum.js
+++ api/picsum.js
@@ -1,7 +1,10 @@
-const endPoint = "https://picsum.photos/v2/list?limit=8";
 const imageList = document.querySelector(".images");
 const loadMore = document.querySelector(".load-more");
-let images = [];
+const loading = document.querySelector(".image-loader");
+loadMore.style.display = "none";
+const limit = 4;
+const endPoint = `https://picsum.photos/v2/list?limit=${limit}`;
+let page = 1;
 function imageTemplate(url) {
    const template = ` <div class="image-item">
    <img src="${url}" alt="">
@@ -9,17 +12,21 @@ function imageTemplate(url) {
    imageList.insertAdjacentHTML("beforeend", template);
 }
 
-async function fetchImage() {
-   const response = await fetch(endPoint);
-   images = await response.json();
+async function fetchImage(page = 1) {
+   loading.style.display = "block";
+   const response = await fetch(`${endPoint}&page=${page}`);
+   const images = await response.json();
    if (images.length > 0 && Array.isArray(images)) {
+      loading.style.display = "none";
+      loadMore.style.display = "block";
       [...images].forEach((item) => {
          imageTemplate(item.download_url);
-      })
+      });
    }
 }
 async function handleLoadMore() {
-   console.log("load more")
+   page++;
+   await fetchImage(page);
 }
 loadMore.addEventListener("click", handleLoadMore);
 fetchImage();
